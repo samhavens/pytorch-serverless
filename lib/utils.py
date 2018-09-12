2bc998f0c3e1d48bf991c8f400edfcfa5d0f8b39
Sam Havens <samhavens@gmail.com>
2018-09-11 17:51:45 -0700

    remove cv2 dep, add boto3

--- lib/utils.py
+++ lib/utils.py
@@ -1,6 +1,5 @@
 import os
 import boto3
-import cv2
 import numpy as np
 import urllib.request
 
@@ -26,40 +25,3 @@ def get_labels(path):
 		labels = [line.strip() for line in f.readlines()]
 		f.close()
 	return labels
-
-
-def open_image_url(url):
-	"""  Opens an image using OpenCV from a URL.
-	:param url: url path of the image
-	:return: the image in RGB format as numpy array of floats normalized to range between 0.0 - 1.0
-	"""
-	flags = cv2.IMREAD_UNCHANGED+cv2.IMREAD_ANYDEPTH+cv2.IMREAD_ANYCOLOR
-	url = str(url)
-	resp = urllib.request.urlopen(url)
-	try:
-		im = np.asarray(bytearray(resp.read()))
-		im = cv2.imdecode(im, flags).astype(np.float32)/255
-		if im is None: raise OSError(f'File from url not recognized by opencv: {url}')
-		return im
-	except Exception as e:
-		raise OSError(f'Error handling image from url at: {url}') from e
-
-
-def open_image(path):
-	""" Opens an image using OpenCV given the file path.
-	:param path: the file path of the image
-	:return: the image in RGB format as numpy array of floats normalized to range between 0.0 - 1.0
-	"""
-	flags = cv2.IMREAD_UNCHANGED+cv2.IMREAD_ANYDEPTH+cv2.IMREAD_ANYCOLOR
-	path = str(path)
-	if not os.path.exists(path):
-		raise OSError(f'No such file or directory: {path}')
-	elif os.path.isdir(path):
-		raise OSError(f'Is a directory: {path}')
-	else:
-		try:
-			im = cv2.imread(str(path), flags).astype(np.float32)/255
-			if im is None: raise OSError(f'File not recognized by opencv: {path}')
-			return cv2.cvtColor(im, cv2.COLOR_BGR2RGB)
-		except Exception as e:
-			raise OSError(f'Error handling image at: {path}') from e
